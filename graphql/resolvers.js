a276ba4ce8b0beccf868eb43dad0a6c54eb99d4d
darkbright <foxtori8946@gmail.com>
2019-07-17 16:09:16 +0900

    node-fetch => axios

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,10 @@
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
+import { getMovies, getMovie, getSuggestions } from "./db";
 
 const resolvers = {
   Query: {
-    movies: (_, { rating, limit }) => getMovies(limit, rating)
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id)
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
   }
 };
 
