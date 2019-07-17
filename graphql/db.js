f347f91206b30ddef88e72892687648d4cd6cb52
darkbright <foxtori8946@gmail.com>
2019-07-17 15:02:49 +0900

    Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -27,3 +27,23 @@ export const getById = id => {
   const filteredMovies = movies.filter(movie => movie.id === id);
   return filteredMovies[0];
 };
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: movies.length,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
+}
